7f9cdc73b5134ad784cff068a185641f04657daf
Данил <@DESKTOP-5U25SJE>
2016-06-19 21:16:40 +0500

    Add project files.

---
+++ Figures/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public class Triangle
+    {
+        /// <summary>
+        /// Calculate square of triangle by three sides
+        /// </summary>
+        /// <param name="a">First side</param>
+        /// <param name="b">Second side</param>
+        /// <param name="c">Third side</param>
+        /// <returns>if values are wrong then returns 0 else returns square of triangle</returns>
+        public double Square(double a, double b, double c)
+        {
+            //Check input
+            if ((a + b <= c) || (a + c <= b) || (c + b <= a) ||
+                (a <= 0) || (b <= 0) || (c <= 0))
+                return 0;
+            double pd2 = (a + b + c)/2;
+            double s = Math.Sqrt((pd2*(pd2 - a)*(pd2 - b)*(pd2 - c)));
+            return s;
+        }
+    }
+}
---
+++ UnitTestFigures/UnitTestTriangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Figures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestFigures
+{
+    [TestClass]
+    public class UnitTestTriangle
+    {
+        private Figures.Triangle triangle = new Triangle();
+
+        /// <summary>
+        /// Check for wrong values. Excpect bad result.
+        /// </summary>
+        [TestMethod]
+        public void TestSquare()
+        {
+            double s = 0;
+
+            //Check for negative values
+            s = triangle.Square(-2, 1, 2);
+            Assert.AreEqual(0, s);
+            s = triangle.Square(2, -1, 2);
+            Assert.AreEqual(0, s);
+            s = triangle.Square(2, 1, -2);
+            Assert.AreEqual(0, s);
+
+            //Check for boundary values
+            s = triangle.Square(0, 1, 2);
+            Assert.AreEqual(0, s);
+            s = triangle.Square(2, 0, 2);
+            Assert.AreEqual(0, s);
+            s = triangle.Square(2, 1, 0);
+            Assert.AreEqual(0, s);
+            s = triangle.Square(0, 0, 0);
+            Assert.AreEqual(0, s);
+
+            //Check for relative wrong values
+            s = triangle.Square(1, 2, 3);
+            Assert.AreEqual(0, s);
+            s = triangle.Square(1, 1, 2);
+            Assert.AreEqual(0, s);
+            s = triangle.Square(1.2, 5.986, 2.367);
+            Assert.AreEqual(0, s);
+
+            //Check for good values. Compare double with elipson.
+            s = triangle.Square(3, 4, 5);
+            Assert.IsTrue(Math.Abs(s - 6) <= Double.Epsilon);
+            s = triangle.Square(6, 8, 10);
+            Assert.IsTrue(Math.Abs(s - 24) <= Double.Epsilon);
+            s = triangle.Square(4294967295, 1, 4294967295);
+            Assert.IsTrue(Math.Abs(s - 2147483647.4999999999854480847682) <= Double.Epsilon);
+            s = triangle.Square(2147483647, 1, 2147483647);
+            Assert.IsTrue(Math.Abs(s - 1073741823.4999999999708961695297) <= Double.Epsilon);
+        }
+    }
+}
